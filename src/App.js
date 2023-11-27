29f458162a0678bf9a86fc160f3f5a2b7304daef
niraj <nirajnsp5@gmail.com>
2023-11-28 00:07:55 +0530

    hamburger functional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import Navbar from "./components/Navbar"
-import LeftSideBar from "./components/LeftSidebar"
+import Navbar from "./components/Navbar";
+import LeftSideBar from "./components/LeftSidebar";
+import Drag from "./components/Drag";
+import { useState } from "react";
 export default function App() {
+  const [isLeftSidebarOpen, setLeftSidebarOpen] = useState(false);
+  const isOpen = ()=>{
+    setLeftSidebarOpen(!isLeftSidebarOpen);
+  }
   return (
     <div className="h-full w-full p-0 m-0">
-      <Navbar></Navbar>
-      <LeftSideBar isOpen="" ></LeftSideBar>
+      <div className="h-full w-full flex flex-col justify-between">
+        <div className="w-full h-full">
+          <Navbar onToggleLeftSidebar={isOpen}></Navbar>
+        </div>
+        <div className="w-full h-full flex flex-row">
+          <div>
+            <LeftSideBar isOpen={isLeftSidebarOpen} ></LeftSideBar>
+          </div>
+          <div className="w-full h-full flex flex-1">
+          <Drag></Drag>
+          </div>
+        </div>
+        <div className="w-full h-5 border-t"></div>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,10 @@
-import { useState } from "react";
 
-export default function Navbar() {
-  const [hamburger, setHamburger] = useState(false);
-  const isOpen = ()=>{
-    setHamburger(!hamburger);
-    console.log(hamburger);
-  }
-
+export default function Navbar({onToggleLeftSidebar}) {
   return (
-    <div className="flex flex-row  w-full h-16 justify-between border-b border-blue-500 items-center" >
+    <div className="flex flex-row  w-full h-16 justify-between border-b items-center transition duration-700 ease-in-out" >
       <div className="flex flex-row" id="left">
-        <div className="p-2 hover:bg-slate-400 cursor-pointer rounded-sm" onClick={isOpen}>
-          <svg  className="fill-current text-red-700" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+        <div className="p-2 hover:bg-slate-200 cursor-pointer rounded-sm" onClick={onToggleLeftSidebar}>
+          <svg  className="fill-current text-gray-700" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
         </div>
         <div className="">Logo</div>
       </div>
