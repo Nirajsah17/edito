218061729a9ecc04ac884afa81904ab5d060de21
niraj <nirajnsp5@gmail.com>
2023-11-29 21:33:27 +0530

    Fix the layout | add the logo of apps

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import Drag from "./components/Drag";
 import { useState } from "react";
 export default function App() {
   const [isLeftSidebarOpen, setLeftSidebarOpen] = useState(false);
-  const isOpen = ()=>{
+  const isOpen = () => {
     setLeftSidebarOpen(!isLeftSidebarOpen);
   }
   return (
-    <div className="h-full w-full p-0 m-0">
-      <div className="h-full w-full flex flex-col justify-between">
-        <div className="w-full h-full">
-          <Navbar onToggleLeftSidebar={isOpen}></Navbar>
-        </div>
-        <div className="w-full h-full flex flex-row">
-          <div>
-            <LeftSideBar isOpen={isLeftSidebarOpen} ></LeftSideBar>
-          </div>
-          <div className="w-full h-full flex flex-1">
-          <Drag></Drag>
-          </div>
-        </div>
-        <div className="w-full h-5 border-t"></div>
+    <div className="flex flex-col justify-between h-screen w-full">
+      <div className="flex flex-row w-full h-12">
+        <Navbar onToggleLeftSidebar={isOpen}></Navbar>
       </div>
+      <div className="flex flex-row flex-1">
+        <div className="flex flex-row">
+          <div>
+            <LeftSideBar isOpen={isLeftSidebarOpen}></LeftSideBar>
+          </div>
+          <div>
+            hhh
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row justify-center h-12 w-full border">footer</div>
     </div>
   )
 }
--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -1,6 +1,7 @@
 export default function LeftSideBar(props) {
   return (
-    props.isOpen ? <div className="flex flex-col w-72 h-screen justify-between border-r  items-center">
+    props.isOpen ? 
+    <div className="flex flex-col w-72 h-full border-r items-center">
     <div className="" id="profile-card"></div>
     <div>Project</div>
     <div>Trash</div>
