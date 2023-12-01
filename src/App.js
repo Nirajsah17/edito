de616987143e9ec8c002a4b3302dea10f0098835
niraj <nirajnsp5@gmail.com>
2023-12-01 21:58:04 +0530

    signup created

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import Navbar from "./components/Navbar";
 import LeftSideBar from "./components/LeftSidebar";
+import SignUp from "./components/SignUp";
 import { useState } from "react";
 import Store from "./storeDB";
 
 export default function App() {
   const UserStore = new Store();
-  UserStore.addUser("email",{ id: 1, usename: "niraj", email: "niraj@yml.com" });
+  // UserStore.addUser("email",{ id: 1, usename: "niraj", email: "niraj@yml.com" });
   const [isLeftSidebarOpen, setLeftSidebarOpen] = useState(false);
+  const [isSignUpOpen, setSignUpOpen] = useState(false);
+
   const isOpen = () => {
     setLeftSidebarOpen(!isLeftSidebarOpen);
+  }
+
+  const isSignUp = () => {
+    setSignUpOpen(!isSignUpOpen);
+    console.log(isSignUpOpen);
   }
 
   return (
     <div className="flex flex-col justify-between h-screen w-full">
       <div className="flex flex-row w-full h-12">
-        <Navbar onToggleLeftSidebar={isOpen}></Navbar>
+        <Navbar onToggleLeftSidebar={isOpen} onSignUpOpen={isSignUp}></Navbar>
       </div>
       <div className="flex flex-row flex-1">
         <div className="flex flex-row">
           <div>
             <LeftSideBar isOpen={isLeftSidebarOpen}></LeftSideBar>
           </div>
-          <div>
-            hhh
+          <div className="flex relative">
+            {isSignUpOpen ?
+              <SignUp onSignUpOpen={isSignUp}></SignUp> : ''}
           </div>
         </div>
       </div>
