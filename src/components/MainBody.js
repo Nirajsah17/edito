2c707d7c32d3e4654ffa4a40f32e034f6bc9f9b0
niraj <nirajnsp5@gmail.com>
2023-12-08 00:11:10 +0530

    first cut text highlighter and editor added

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,30 +1,55 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import 'prismjs/themes/prism.css';
+import Prism from 'prismjs';
+import '../custom.css';
 
 function MainBody() {
+  const [text, setText] = useState("");
+  const code = useRef(null);
+  const editing = useRef(null);
+
+  const syncScroll = () => {
+    code.current.scrollTop = editing.current.scrollTop;
+    code.current.scrollLeft = editing.current.scrollLeft;
+    console.log(editing.current.scrollTop);
+  }
+
+  const handleText = (e) => {
+    const newText = e.target.value;
+    // For new line support 
+    newText.replace(new RegExp("&", "g"), "&").replace(new RegExp("<", "g"), "<");
+    setText(newText);
+    code.current.scrollTop = editing.current.scrollTop;
+    code.current.scrollLeft = editing.current.scrollLeft;
+  };
+
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [text]);
+
   return (
     <>
       <div className="w-full h-full flex flex-col">
-        <div className="flex flex-row h-9 border-b bg-gray-100">
-          <div className="flex flex-row justify-between border-r hover:bg-gray-200 cursor-pointer">
-            <div className="p-2">main.js</div>
-            <div className="p-2 cursor-pointer hover:bg-gray-300">X</div>
-          </div>
-          <div className="flex flex-row justify-between border-r bg-gray-300 cursor-pointer">
-            <div className="p-2">index.js</div>
-            <div className="p-2 cursor-pointer hover:bg-gray-300">X</div>
-          </div>
-        </div>
-        <div className="w-full flex flex-1">
+        <div className="flex flex-1">
           <div className="flex flex-1">
-            <div className="w-6 p-2 border-r">
-              1.
+            <div className="w-6 p-2 border-r h-full overflow-y-auto">1.</div>
+            <div className="flex flex-1">
+              <textarea id="editing" spellCheck="false" ref={editing}
+                onChange={handleText}
+                value={text}
+                onScroll={syncScroll}
+              ></textarea>
+              <pre id="highlighting" aria-hidden="true">
+                <code className="language-javascript" ref={code}>
+                  {text}
+                </code>
+              </pre>
             </div>
-            <div className="flex flex-1" contentEditable="true"></div>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default MainBody;
