b69738cf7b5c7ea328b716ea8f4b93c0c5f4f9ca
niraj <nirajnsp5@gmail.com>
2024-04-02 23:09:15 +0530

    github added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -47,8 +47,8 @@ export default function Navbar({
         </div>
         <div className="flex flex-row">
           <label className="inline-flex items-center me-5 cursor-pointer">
-          <input type="checkbox" onChange={handleMode} value="" className="sr-only peer"></input>
-          <div className="relative w-11 h-6 rounded-full peer dark:bg-gray-200 peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-900"></div>
+            <input type="checkbox" onChange={handleMode} value="" className="sr-only peer"></input>
+            <div className="relative w-11 h-6 rounded-full peer dark:bg-gray-200 peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-900"></div>
           </label>
           {userName ? (
             <div className="m-1 flex flex-row justify-center relative">
@@ -79,6 +79,11 @@ export default function Navbar({
                 >
                   Login
                 </button>
+                <a href="https://github.com/Nirajsah17/edito/tree/develop" target="_blank" className="flex justify-between items-center">
+                  <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github v-align-middle color-fg-default">
+                    <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+                  </svg>
+                </a>
               </div>
             </>
           )}
